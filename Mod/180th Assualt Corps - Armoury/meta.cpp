65d473eb95b64e494d997fb9392bc0c868be2917
Adam Lambert (Wallace) <57895953+AdamLambert001@users.noreply.github.com>
2021-12-05 12:48:55 -0600

    InitialSetUp

---
+++ Mod/180th Assualt Corps - Armoury/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 1776250663;
+name = "180th Assault Corps - Armory";
+timestamp = 5249396465597208145;
---
+++ Mod/180th Assualt Corps - Armoury/mod.cpp
@@ -0,0 +1,6 @@
+author = "Wyvern Studios";
+name = "180th Assault Corps - Armory";
+picture = ""; 
+logo = "logo.paa";
+overview = "";
+version = 0.9;

593cc16e812e77cac4acd7c60ccc76e07bfd24ca
AL229568 <AL229568@falmouth.ac.uk>
2021-12-16 19:35:29 +0000

    Another quickie fixy of cfgpatches

--- Mod/WS_UNITS/FC_BF_180DEF/CfgPatches.hpp
+++ Mod/WS_UNITS/FC_BF_180DEF/CfgPatches.hpp
@@ -35,9 +35,7 @@ class CfgPatches {
         };
         weapons[] = {};
         requiredVersion = 1.62;
-        requiredAddons[] = {
-            ""
-        };
+        requiredAddons[] = {};
         author = "Wyvern Studios ";
         authors[] = {"Poop Mastwe Raccoon","Roach","Wallace"};
     };

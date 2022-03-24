8cfdc9beea9191f37c1350185f6b3d9ef004cb9b
Adam Lambert <al229568@falmouth.ac.uk>
2022-03-24 17:19:57 +0000

    Vehicle update, Reaper pilot helm
    Moved kroners LAAT into archive

--- Mod/WS_A_CUSTOMS/config.cpp
+++ Mod/WS_A_CUSTOMS/config.cpp
@@ -288,6 +288,14 @@ class CfgWeapons
 			"WS_A_CUSTOMS\data\helmets\180th_Reaper_Helmet.paa"
 		};
 	};
+	class 180th_Reaper_Pilot_Helmet: SWLB_clone_pilot_P2_helmet
+	{
+		displayName="[180th Customs] Clone trooper Pilot helmet (Reaper)";
+		hiddenSelectionsTextures[]=
+		{
+			"WS_A_CUSTOMS\data\helmets\180th_Reaper_Pilot_Helmet.paa"
+		};
+	};
     class 180th_Grape_Helmet: SWLB_clone_P2_helmet
 	{
 		displayName="Grape Man Helmet";

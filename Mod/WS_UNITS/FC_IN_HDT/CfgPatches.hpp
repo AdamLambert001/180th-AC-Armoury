c8a72ef8390260843f1d2ef33a39587a6817f926
Adam Lambert <al229568@falmouth.ac.uk>
2022-03-25 23:14:58 +0000

    Racc's WS_Units

---
+++ Mod/WS_UNITS/FC_IN_HDT/CfgPatches.hpp
@@ -0,0 +1,17 @@
+class CfgPatches {
+    class I_HERD {
+        units[] = {
+            "I_HERD_Warrior_01",
+            "I_HERD_Rusher_01",
+            "I_HERD_Chief_01",
+            "I_HERD_Seeker_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F"
+        };
+        author = "CC-3572 ""Cordo""";
+        authors[] = {"CC-3572 ""Cordo"""};
+    };
+};
---
+++ Mod/WS_UNITS/FC_OP_CIS74/CfgPatches.hpp
@@ -0,0 +1,35 @@
+class CfgPatches {
+    class O_CIS74 {
+        units[] = {
+            "O_CIS_74_Rifleman_01",
+            "O_CIS_74_AutoRifleman_01",
+            "O_CIS_74_Heavy_Gunner_01",
+            "O_CIS_74_Medic_01",
+            "O_CIS_74_Rocket_Trooper_01",
+            "O_CIS_74_Rocket_Trooper_AA_01",
+            "O_CIS_74_Marksman_01",
+            "O_CIS_74_Squad_Leader_01",
+            "O_CIS_74_Raider_01",
+            "O_CIS_74_Pilot_01",
+            "O_CIS_74_Vehicle_Crewman_01",
+            "O_CIS_74_Naval_Crewman_01",
+            "O_CIS_74_Naval_Soldier_01",
+            "O_CIS_74_Naval_Officer_01",
+            "O_CIS_74_Facility_Guard_01",
+            "O_CIS_74_Lieutenant_Brigov_01",
+            "O_CIS_74_River_Patrol_Craft_01",
+            "O_CIS_74_River_Patrol_Craft_Armed_01",
+            "O_CIS_74_Security_Droid_01",
+            "O_CIS_74_B1_Jungle_01",
+            "O_CIS_74_B1_AutoRifleman_Jungle_01",
+            "O_CIS_74_BX_series_Security_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F"
+        };
+        author = "CC-3572 ""Cordo""";
+        authors[] = {"CC-3572 ""Cordo"""};
+    };
+};
